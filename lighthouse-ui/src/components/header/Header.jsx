fcd20242ecb2838ac4a7d696e866c10925b29ded
Ben Turner <turner.benjamin88@gmail.com>
2024-07-18 09:30:09 +0100

    BUG: Fixed missing bg img

--- lighthouse-ui/src/components/header/Header.jsx
+++ lighthouse-ui/src/components/header/Header.jsx
@@ -3,17 +3,19 @@ import AuthenticationControls from "./AuthenticationControls";
 
 export default function Header() {
   return (
-    <div className="p-4  shadow-sm grid grid-cols-[1fr_auto]">
+    <div className="p-4 shadow-sm grid grid-cols-[1fr_auto] bg-secondary-50">
       <Link to="/">
-        <h1 className="text-primary-800  tracking-wider text-3xl">
-          L<span className="text-secondary-800 font-light">ightHouse</span>
+        <h1 className="text-primary-800 tracking-wider text-3xl">
+          L
+          <span className="text-secondary-800 font-light text-3xl">
+            ightHouse
+          </span>
         </h1>
       </Link>
 
       <div className="flex flex-row gap-2">
         <AuthenticationControls />
       </div>
-      <div className="absolute inset-0 bg-secondary-50 -z-20" />
     </div>
   );
 }

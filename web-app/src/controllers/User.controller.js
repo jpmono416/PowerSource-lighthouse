c11a2591b6971a8a51f98d413a03b9f7817865d1
Ben Turner <turner.benjamin88@gmail.com>
2024-07-24 09:16:19 +0100

    ADDED: Cookies to FE and BE

--- web-app/src/controllers/User.controller.js
+++ web-app/src/controllers/User.controller.js
@@ -1,113 +1,155 @@
 import UserService from "../services/User.service.js";
 
 export default class UserController {
-    // Details
-    static createUser = async (req, res) => {
-        try {
-            if (!req.body) return res.status(400).json({ error: "Invalid user" });
+  // Details
+  static createUser = async (req, res) => {
+    try {
+      if (!req.body) return res.status(400).json({ error: "Invalid user" });
 
-            const { user, token } = await UserService.createUser(req.body);
+      const user = await UserService.createUser(req.body);
 
-            res.status(201).json({ user, token });
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-    static getUserByEmail = async (req, res) => {
-        try {
-            if (!req.params.email) return res.status(400).json({ error: "Invalid email" });
-            const user = await UserService.getUserByEmail(req.params.email);
-            if (!user) return res.status(404).json({ error: "User not found" });
+  static getUserByEmail = async (req, res) => {
+    try {
+      if (!req.params.email)
+        return res.status(400).json({ error: "Invalid email" });
+      const user = await UserService.getUserByEmail(req.params.email);
+      if (!user) return res.status(404).json({ error: "User not found" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-    static getUserById = async (req, res) => {
-        try {
-            if (!req.params) return res.status(400).json({ error: "Invalid user ID" });
-            const user = await UserService.getUserById(req.params.id);
-            if (!user) return res.status(404).json({ error: "User not found" });
+  static getUserById = async (req, res) => {
+    try {
+      if (!req.params)
+        return res.status(400).json({ error: "Invalid user ID" });
+      const user = await UserService.getUserById(req.params.id);
+      if (!user) return res.status(404).json({ error: "User not found" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-    // Credentials
-    static loginUser = async (req, res) => {
-        try {
-            if (!req.body.email || !req.body.password)
-                return res.status(400).json({ error: "Invalid email or password" });
+  // Credentials
+  static loginUser = async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.password)
+        return res.status(400).json({ error: "Invalid email or password" });
 
-            const user = await UserService.loginUser(req.body.email, req.body.password);
-            if (!user) return res.status(404).json({ error: "User not found" });
+      const { user, token } = await UserService.loginUser(
+        req.body.email,
+        req.body.password
+      );
+      if (!user) return res.status(404).json({ error: "User not found" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      this.#setCookie(res, token);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-    static changePassword = async (req, res) => {
-        try {
-            if (!req.body || !req.body.email || !req.body.password || !req.body.newPassword)
-                return res
-                    .status(400)
-                    .json({ error: "Invalid email, old password or new password" });
+  static signOut = async (_, res) => {
+    res.clearCookie("jwt");
+    res.status(204).send();
+  };
 
-            const user = await UserService.changePassword(
-                req.body.email,
-                req.body.password,
-                req.body.newPassword
-            );
-            if (!user) return res.status(404).json({ error: "User not found" });
+  static changePassword = async (req, res) => {
+    try {
+      if (
+        !req.body ||
+        !req.body.email ||
+        !req.body.password ||
+        !req.body.newPassword
+      )
+        return res
+          .status(400)
+          .json({ error: "Invalid email, old password or new password" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      const user = await UserService.changePassword(
+        req.body.email,
+        req.body.password,
+        req.body.newPassword
+      );
+      if (!user) return res.status(404).json({ error: "User not found" });
 
-    // Roles
-    static getRoles = async (req, res) => {
-        try {
-            const roles = await UserService.getRoles();
-            res.status(200).json(roles);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-    static addRole = async (req, res) => {
-        try {
-            if (!req.body.email || !req.body.role)
-                return res.status(400).json({ error: "Invalid email or role" });
+  // Roles
+  static getRoles = async (req, res) => {
+    try {
+      const roles = await UserService.getRoles();
+      res.status(200).json(roles);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-            const user = await UserService.addRole(req.body.email, req.body.role);
-            if (!user) return res.status(404).json({ error: "User not found" });
+  static addRole = async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.role)
+        return res.status(400).json({ error: "Invalid email or role" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      const user = await UserService.addRole(req.body.email, req.body.role);
+      if (!user) return res.status(404).json({ error: "User not found" });
 
-    static removeRole = async (req, res) => {
-        try {
-            if (!req.body.email || !req.body.role)
-                return res.status(400).json({ error: "Invalid email or role" });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
 
-            const user = await UserService.removeRole(req.body.email, req.body.role);
-            if (!user) return res.status(404).json({ error: "User not found" });
+  static removeRole = async (req, res) => {
+    try {
+      if (!req.body.email || !req.body.role)
+        return res.status(400).json({ error: "Invalid email or role" });
 
-            res.status(200).json(user);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    };
+      const user = await UserService.removeRole(req.body.email, req.body.role);
+      if (!user) return res.status(404).json({ error: "User not found" });
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+  static requireLoggedIn = async (req, res, next) => {
+    try {
+      if (!req?.cookies?.jwt)
+        return res.status(401).json({ error: "Unauthorised" });
+      req.user = await UserService.validateToken(req.cookies.jwt);
+      next();
+    } catch (err) {
+      res.status(401).json({ error: "Unauthorised" });
+    }
+  };
+
+  static requireAdminRole = async (req, res, next) => {
+    if (!req.user || !req.user.roles?.includes("admin"))
+      return res.status(403).json({ error: "Forbidden" });
+    next();
+  };
+
+  static #setCookie = (res, token) => {
+    res.cookie("jwt", token, {
+      maxAge: parseInt(process.env.COOKIE_EXPIRES_IN),
+      secure: true,
+      sameSite: "None",
+      partitioned: true,
+    });
+  };
 }
--- web-app/src/services/User.service.js
+++ web-app/src/services/User.service.js
@@ -1,55 +1,85 @@
-import bcrypt from "bcrypt";
 import { defineUserModel } from "../models/User.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export default class UserService {
-    static async getUserModel() {
-        if (!this.UserModel) {
-            this.UserModel = defineUserModel();
-        }
-        return this.UserModel;
+  static async getUserModel() {
+    if (!this.UserModel) {
+      this.UserModel = defineUserModel();
     }
+    return this.UserModel;
+  }
 
-    static async createUser(userDetails) {
-        const { email, password, username, roles } = userDetails;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        try {
-            const UserModel = await this.getUserModel();
-            const user = await UserModel.create({
-                email,
-                password: hashedPassword,
-                username,
-                roles,
-            });
-            return user;
-        } catch (error) {
-            console.error("Error creating user:", error);
-            throw error;
-        }
+  static async createUser(userDetails) {
+    const { email, password, username, roles } = userDetails;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+      const UserModel = await this.getUserModel();
+      const user = await UserModel.create({
+        email,
+        password: hashedPassword,
+        username,
+        roles,
+      });
+      return user;
+    } catch (error) {
+      console.error("Error creating user:", error);
+      throw error;
     }
+  }
 
-    static async getUserByEmail(email) {
-        try {
-            const UserModel = await this.getUserModel();
-            const user = await UserModel.findOne({ where: { email } });
-            return user;
-        } catch (error) {
-            console.error("Error retrieving user by email:", error);
-            throw error;
-        }
+  static async getUserByEmail(email) {
+    try {
+      const UserModel = await this.getUserModel();
+      const user = await UserModel.findOne({ where: { email } });
+      return user;
+    } catch (error) {
+      console.error("Error retrieving user by email:", error);
+      throw error;
     }
+  }
 
-    static async loginUser(email, password) {
-        try {
-            const user = await this.getUserByEmail(email);
-            if (!user) return null;
+  static async loginUser(email, password) {
+    try {
+      const user = await this.getUserByEmail(email);
+      if (!user) return {};
 
-            const isValidPassword = await bcrypt.compare(password, user.password);
-            if (!isValidPassword) return null;
-
-            return user;
-        } catch (error) {
-            console.error("Error logging in user:", error);
-            throw error;
-        }
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) return {};
+      const token = this.#getToken(user.id);
+      return { user, token };
+    } catch (error) {
+      console.error("Error logging in user:", error);
+      throw error;
     }
+  }
+
+  static validateToken = async (token) => {
+    try {
+      const decodedToken = this.#decodeToken(token);
+      const UserModel = await this.getUserModel();
+      const userDocument = await UserModel.findOne({
+        where: { id: decodedToken.userId },
+      });
+      if (!userDocument) throw new Error();
+      return userDocument;
+    } catch (err) {
+      throw new Error();
+    }
+  };
+
+  static #decodeToken = (token) => {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET_KEY);
+    } catch (err) {
+      throw new Error();
+    }
+  };
+
+  static #getToken = (userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+    return token;
+  };
 }

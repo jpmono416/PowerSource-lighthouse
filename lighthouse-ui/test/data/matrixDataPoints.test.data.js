2d562323f085dd6ac01cf1169008ca95ea667e35
Ben Turner <turner.benjamin88@gmail.com>
2024-08-05 09:27:10 +0100

    PASSED: Gartner Quadrant tests

---
+++ lighthouse-ui/test/data/matrixDataPoints.test.data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    name: "Model 1",
+    x: 50,
+    y: 50,
+    link: "/model/1",
+  },
+  {
+    id: 2,
+    name: "Model 2",
+    x: 100,
+    y: 100,
+    link: "/model/2",
+  },
+  {
+    id: 3,
+    name: "Model 3",
+    x: 0,
+    y: 0,
+  },
+];
